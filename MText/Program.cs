b6ad699239e859d7c5069a666338ab86e1d383e9
Minamin <minamin37345@gmail.com>
2021-11-22 01:43:19 +0900

    Tested new methods.

--- MText/Program.cs
+++ MText/Program.cs
@@ -6,7 +6,17 @@ namespace MText
     {
         static void Main(string[] args)
         {
+            char ca = 'a';
+            char cb = 'A';
+            string sa = "Hea llo";
+            string sb = "he llo";
             Console.WriteLine("Hello World!");
+            Console.WriteLine(MText.IsEqual(ca, cb));//True
+            Console.WriteLine(MText.IsEqual(sa, sb));//True
+            ca = 'b';
+            sa = "ggga";
+            Console.WriteLine(MText.IsEqual(ca, cb));//False
+            Console.WriteLine(MText.IsEqual(sa, sb));//False
         }
     }
 }
