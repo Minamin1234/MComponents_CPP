732c9c09e44e1138606e2bd9c2e05c1cb9d5710c
Minamin <minamin37345@gmail.com>
2021-12-23 02:45:30 +0900

    Create project/programs.

---
+++ MCommandComponent/MCommandComponent.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MCommandComponent
+{
+    public class Error : Exception
+    {
+        public Error() : base() { }
+        public Error(string message) : base(message) { }
+    }
+
+    public static class MCommandReader
+    {
+        public static char SeparationChr = ' ';
+        public static IEnumerable<string> GetArgs(string command,char sprtchr)
+        {
+            string cword = "";
+            foreach (var chr in command)
+            {
+                if(chr == sprtchr)
+                {
+                    yield return cword;
+                    cword = "";
+                }
+                else
+                {
+                    cword += chr;
+                }
+            }
+            yield return cword;
+        }
+
+        public static IEnumerable<string> GetArgs(string command)
+        {
+            string cword = "";
+            foreach (var chr in command)
+            {
+                if (chr == SeparationChr)
+                {
+                    yield return cword;
+                    cword = "";
+                }
+                else
+                {
+                    cword += chr;
+                }
+            }
+            yield return cword;
+        }
+    }
+
+    public static class MCommandSearcher
+    {
+
+    }
+
+    public class MCMDModule
+    {
+        public string ModuleName = "";
+        public MCMDModule() { }
+        public virtual bool ExecuteCommand(List<string> CArgs) { return true; }
+    }
+}
---
+++ Test_Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using MCommandComponent;
+
+namespace Test_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string cmd = Console.ReadLine();
+                List<string> Args = new List<string>(MCommandReader.GetArgs(cmd, ' '));
+                foreach (var arg in Args)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
