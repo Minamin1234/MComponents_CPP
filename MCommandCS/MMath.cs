92f7b2a3d13e29e6182efeddddda5ef0b3958e5b
Minamin <minamin37345@gmail.com>
2022-01-16 17:17:47 +0900

    MMathクラスのコマンドを追加

--- MCommandCS/MMath.cs
+++ MCommandCS/MMath.cs
@@ -11,6 +11,18 @@ namespace MCommandCS
         public MMath()
         {
             this.ModuleName = "math";
+            this.Commands.Add("abs");
+            this.Commands.Add("sin");
+            this.Commands.Add("cos");
+            this.Commands.Add("tan");
+            this.Commands.Add("rad");
+            this.Commands.Add("pow");
+            this.Commands.Add("sqrt");
+            this.Commands.Add("max");
+            this.Commands.Add("min");
+            this.Commands.Add("pi");
+            this.Commands.Add("e");
+            this.Commands.Add("help");
         }
     }
 }
