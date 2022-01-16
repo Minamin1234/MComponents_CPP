2c3d4b094b88a3862c7e9ab06fa16ca9ff73200d
Minamin <minamin37345@gmail.com>
2022-01-16 15:46:24 +0900

    MCommandクラスの関数を実装

--- MCommandCS/MCommandCS.cs
+++ MCommandCS/MCommandCS.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace MCommandCS
 {
-    public class MCommandCS
+    public class MCommand
     {
+        public string Version = "v1.0";
+        protected List<MModule> Modules;
+        protected List<string> DefaultCommands;
+        protected string ModuleSprt = ".";
+        protected string SprtInArgs = ",";
+
+        public MCommand()
+        {
+
+        }
+
+        virtual protected void Initialize()
+        {
+
+        }
+
+        public void IncludeNewModule(MModule newmodule)
+        {
+            if (newmodule == null) throw new ArgumentNullException();
+            this.Modules.Add(newmodule);
+        }
+
+        public void Run()
+        {
+
+        }
+
+        public List<string> DecodeArgs(string words)
+        {
+            var args = new List<string>();
+            return args;
+        }
+
+        public void ShowAllModuleCommandInfo()
+        {
+
+        }
+
+        public void ShowAllDefaultCommands()
+        {
+
+        }
+
+        public void ExecuteCommand(List<string> args)
+        {
+
+        }
+
     }
 }
